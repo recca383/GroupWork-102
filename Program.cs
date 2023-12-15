68a98e9db4026e58bbd6815f7f5e52007148e4c1
Joko Comia <jokocomia05@gmail.com>
2023-12-15 22:30:42 +0800

    Made the Menu Interactive

--- Program.cs
+++ Program.cs
@@ -2,34 +2,203 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to Group Speed Rev!!");
-        Console.WriteLine("\n1. Histogram \n2. Smart Lily \n3. Back to the Past \n4. Hospital \n5. Butterly \n6. Stop Number ");
-        Console.Write("\nSelect a program to run: ");
-        int response = Convert.ToInt32(Console.ReadLine());
 
-        switch (response){
-            case 1:
+        string[] Options = { "Histogram", "Smart Lily", "Back to the Past", "Hospital", "Butterfly", "Stop Number", "Exit" };
+        int selectedOption = 0;         
+        ConsoleKeyInfo keyInfo;
+
+        do
+        {
+            Console.Clear();
+            
+            int maxLength = Options.Max(s => s.Length);
+            Console.WriteLine("Welcome to the Game Selector!!");
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];
+
+                if (i == selectedOption)
+                {                       
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.WriteLine($" {currentOption.PadRight(maxLength)}");
+                }
+                else
+                {                      
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine($"   {currentOption.PadRight(maxLength)}");
+                }
+            }
+            Console.ResetColor();
+
+            Console.Beep();
+
+            keyInfo = Console.ReadKey(true);
+
+            // Update selectedOption based on arrow keys
+
+            if (keyInfo.Key == ConsoleKey.UpArrow)
+            {
+                selectedOption--;
+                if (selectedOption == -1)
+                {
+                    selectedOption = Options.Length - 1;
+                }
+            }
+            else if (keyInfo.Key == ConsoleKey.DownArrow)
+            {
+                selectedOption++;
+                if (selectedOption == Options.Length)
+                {
+                    selectedOption = 0;
+                }
+            }
+
+        } while (keyInfo.Key != ConsoleKey.Enter);
+
+        switch (selectedOption)
+        {
+            case 0:
+                Console.CursorVisible = true;
                 Histogram();
                 break;
+            case 1:
+                Console.CursorVisible = true;
+                SmartLily();
+                break;
             case 2:
+                Console.CursorVisible = true;
+                BackToThePast();
                 break;
             case 3:
-                break;
-            case 4:
+                Console.CursorVisible = true;
                 Hospital();
                 break;
-            case 5:
+            case 4:
+                Console.CursorVisible = true;
                 Butterfly();
-                break; 
-            case 6:
                 break;
-
+            case 5:
+                Console.CursorVisible = true;
+                StopNumber();
+                break;
+            case 6:
+                Console.CursorVisible = true;
+                ExitGame();
+                break;
         }
 
-        Console.ReadKey();
+
     }
+    static void Histogram()
+    {
+        bool run = false;
+        List<int> All_Inputs = new List<int>();
+        List<float> p1 = new List<float>();
+        List<float> p2 = new List<float>();
+        List<float> p3 = new List<float>();
+        List<float> p4 = new List<float>();
+        List<float> p5 = new List<float>();
+        do
+        {
+            int input = 0;
+
+            try
+            {
+                Console.Write("Input an integer: ");
+                input = Convert.ToInt32(Console.ReadLine());
+                All_Inputs.Add(input);
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Enter integer only ");
+
+            }
+            Console.Write("Add more numbers? (Y/N) : ");
+            char response = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            run = (response == 'Y') ? true : false;
+
+
+        } while (run);
+
+        All_Inputs.ToArray();
+        foreach (int i in All_Inputs)
+        {
+            if (i >= 800) p5.Add(i);
+            else if (i >= 600) p4.Add(i);
+            else if (i >= 400) p3.Add(i);
+            else if (i >= 200) p2.Add(i);
+            else if (i < 200) p1.Add(i);
+        }
+        Console.WriteLine("0 - 199 = {0}%", HistogramPercentage(p1.Count, All_Inputs.Count));
+        Console.WriteLine("200 - 399 = {0}%", HistogramPercentage(p2.Count, All_Inputs.Count));
+        Console.WriteLine("400 - 599 = {0}%", HistogramPercentage(p3.Count, All_Inputs.Count));
+        Console.WriteLine("600 - 799 = {0}%", HistogramPercentage(p4.Count, All_Inputs.Count));
+        Console.WriteLine("800 above = {0}%", HistogramPercentage(p5.Count, All_Inputs.Count));
+    }
+    static float HistogramPercentage(float count, float total)
+    {
+        return (count / total) * 100;
+    }
+    static void SmartLily()
+    {
+
+    }
+
+    static void BackToThePast()
+    {
+
+    }
+
+    static void Hospital()
+    {
+        Console.Write("Enter Number of Days: ");
+        int days = Convert.ToInt32(Console.ReadLine());
+
+        int doctors = 7;
+        int treatedPat = 0;
+        int untreatedPat = 0;
+
+        if ((days >= 1) && (days <= 1000))
+        {
+            for (int period = 1; period <= days; period++)
+            {
+                Console.Write("Enter Number of Patients: ");
+                int currentPat = Convert.ToInt32(Console.ReadLine());
+
+                if ((currentPat >= 0) && (currentPat <= 10000))
+                {
+                    if ((period % 3 == 0) && (untreatedPat > treatedPat))
+                    {
+                        doctors++;
+                    }
+                    if (currentPat > doctors)
+                    {
+                        untreatedPat += currentPat - doctors;
+
+                        treatedPat += doctors;
+                    }
+                    else
+                    {
+                        treatedPat += currentPat;
+                    }
+                }
+                else
+                { break; }
+
+            }
+
+            Console.WriteLine($"Treated patients: {treatedPat}.");
+            Console.WriteLine($"Untreated patients: {untreatedPat}.");
+        }
+    }
+
     static void Butterfly()
     {
+        Console.Clear();
         Console.Write("Input a number: ");
         int n = Convert.ToInt32(Console.ReadLine());
 
@@ -121,100 +290,19 @@
             }
         }
 
-        Console.ReadKey();
+        Console.ReadKey(true);
+        return;
     }
-    static void Hospital()
+
+    static void StopNumber()
     {
-        Console.Write("Enter Number of Days: ");
-        int days = Convert.ToInt32(Console.ReadLine());
 
-        int doctors = 7;
-        int treatedPat = 0;
-        int untreatedPat = 0;
-
-        if ((days >= 1) && (days <= 1000))
-        {
-            for (int period = 1; period <= days; period++)
-            {
-                Console.Write("Enter Number of Patients: ");
-                int currentPat = Convert.ToInt32(Console.ReadLine());
-
-                if ((currentPat >= 0) && (currentPat <= 10000))
-                {
-                    if ((period % 3 == 0) && (untreatedPat > treatedPat))
-                    {
-                        doctors++;
-                    }
-                    if (currentPat > doctors)
-                    {
-                        untreatedPat += currentPat - doctors;
-
-                        treatedPat += doctors;
-                    }
-                    else
-                    {
-                        treatedPat += currentPat;
-                    }
-                }
-                else
-                { break; }
-
-            }
-
-            Console.WriteLine($"Treated patients: {treatedPat}.");
-            Console.WriteLine($"Untreated patients: {untreatedPat}.");
-        }
     }
-    static void Histogram()
+
+    static void ExitGame()
     {
-        bool run = false;
-        List <int> All_Inputs = new List<int>();
-        List <float> p1 = new List<float>();
-        List<float> p2 = new List<float>();
-        List<float> p3 = new List<float>();
-        List<float> p4 = new List<float>();
-        List<float> p5 = new List<float>();
-        do
-        {
-            int input = 0;
-
-            try
-            {
-                Console.Write("Input an integer: ");
-                input = Convert.ToInt32(Console.ReadLine());
-                All_Inputs.Add(input);
-
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine("Enter integer only ");
-                
-            }   
-                Console.Write("Add more numbers? (Y/N) : ");
-                char response = Convert.ToChar(Console.ReadLine().ToUpper());
-
-                run = (response == 'Y') ? true : false;
-           
-
-        } while (run);
-
-        All_Inputs.ToArray();
-        foreach (int i in All_Inputs)
-        {
-            if (i >= 800) p5.Add(i);
-            else if (i >= 600) p4.Add(i);
-            else if (i >= 400) p3.Add(i);
-            else if (i >= 200) p2.Add(i);
-            else if (i < 200) p1.Add(i);
-        }
-        Console.WriteLine("0 - 199 = {0}%", HistogramPercentage(p1.Count,All_Inputs.Count));
-        Console.WriteLine("200 - 399 = {0}%", HistogramPercentage(p2.Count, All_Inputs.Count));
-        Console.WriteLine("400 - 599 = {0}%", HistogramPercentage(p3.Count, All_Inputs.Count));
-        Console.WriteLine("600 - 799 = {0}%", HistogramPercentage(p4.Count, All_Inputs.Count));
-        Console.WriteLine("800 above = {0}%", HistogramPercentage(p5.Count, All_Inputs.Count));
-    }
-    static float HistogramPercentage (float count, float total)
-    {
-        return (count/total) * 100;
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey(true);
+        Environment.Exit(0);
     }
 }
